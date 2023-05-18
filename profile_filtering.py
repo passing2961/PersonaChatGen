d520b5e3b7e50a349e76824838a5ca43b61a30ba
Young-Jun Lee <passing2961@gmail.com>
2023-05-18 17:24:15 +0900

    update codes

--- profile_filtering.py
+++ profile_filtering.py
@@ -1,4 +1,5 @@
 import os
+import re
 import argparse
 import pickle as pc
 from collections import defaultdict
@@ -7,12 +8,132 @@ from tqdm import tqdm
 import stanza
 from transformers import pipeline
 
-from prompt_filter import ProfilePipeline
 from constant import *
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 
+class ProfilePipeline(object):
+    """
+    Filtering pipeline for profile sentences
+    """
+    def __init__(self, models, th, candidate_label):
+        self.org_size = 0
+        self.survival_data = defaultdict(list)
+        self.th = th
+        self.candidate_label = candidate_label
+
+        self.lemma = models['lemma']
+        self.clf = models['clf']
+
+    def calculate_survival_rate(self, after_size):
+        survival_rate = after_size * 100 / self.org_size
+        return round(survival_rate, 1)
+
+    def regex_based_filtering(self, sentences):
+        """
+        If the profile sentence don't match with the regex pattern,
+        then we regard the sentence as an inappropriate generated sentence.
+        So, we filter it out.
+        """
+        result = []
+        
+        for sentence in sentences:
+            sentence = sentence[1:] # remove a whitespace prefix
+
+            delims = [f'\n{i+1}. ' for i in range(1, 5)]
+            splitted_sent = re.split('|'.join(delims), sentence)
+            
+            pattern = '(?P<utter>.*) [\(|\[](?P<attr>.*): (?P<value>.*)[\)|\]]' # [] case should be possible
+            compiled_regex = re.compile(pattern)
+
+            self.org_size += len(splitted_sent)
+            for example in splitted_sent:
+                matched = compiled_regex.match(example)
+                
+                if matched:
+                    result.append(matched.groupdict())
+
+        self.survival_data['origin_data_size'] = [self.org_size, 100]
+        after_size = len(result)
+        survival_rate = self.calculate_survival_rate(after_size)    
+        self.survival_data['filtering:regex'] = [after_size, survival_rate]
+        #self.f.write(f'[Regex] Cumulative Survival Rate (%): {survival_rate}\n')
+        return result
+
+    def explicit_filtering(self, sentences):
+        results = []
+
+        for sentence in sentences:
+            utter = sentence['utter']
+            attr = sentence['attr']
+            value = sentence['value']
+
+            if value == '':
+                continue
+            
+            if value in utter: #and attr == self.candidate_label:
+                results.append(sentence)
+            
+
+        after_size = len(results)
+        survival_rate = self.calculate_survival_rate(after_size)
+        self.survival_data['filtering:explicit'] = [after_size, survival_rate]
+        #self.f.write(f'[Explicit] Cumulative Survival Rate (%): {survival_rate}\n')
+        return results
+
+    def persona_category_filtering(self, sentences):
+        results = []
+        
+        for sentence in sentences:
+            utter = sentence['utter']
+            value = sentence['value']
+
+            output = self.clf(utter, self.candidate_label)
+            if output['scores'][0] < self.th:
+                continue
+            
+            sentence['scores'] = output['scores'][0]
+            results.append(sentence)
+        
+        after_size = len(results)
+        survival_rate = self.calculate_survival_rate(after_size)
+        self.survival_data['filtering:persona_category'] = [after_size, survival_rate]
+        #self.f.write(f'[Preserving] Cumulative Survival Rate (%): {survival_rate}\n')
+        return results
+    
+    
+    def duplication_filtering(self, sentences):
+        results = []
+        dup_results = {}
+        for sentence in sentences:
+            utter, attr, value, scores = sentence.values()
+
+            if utter in dup_results.keys():
+                if dup_results[utter][1] != value:
+                    print(f'Warning: entity value mismatch occurs! Previous value = {dup_results[utter][1]}, Current value = {value} of utterance = {utter}.')
+                    continue
+
+            dup_results[utter] = [attr, value, scores]
+        
+        for k, v in dup_results.items():
+            results.append({'utter': k, 'attr': v[0], 'value': v[1], 'scores': v[2]})
+
+        after_size = len(results)
+        survival_rate = self.calculate_survival_rate(after_size)
+        self.survival_data['filtering:duplication'] = [after_size, survival_rate]
+        return results
+
+    def do_filtering(self, sentences):
+
+        regex_filter_results = self.regex_based_filtering(sentences)
+        explicit_filter_results = self.explicit_filtering(regex_filter_results)
+        persona_category_filter_results = self.persona_category_filtering(explicit_filter_results)
+        
+        duplication_filter_results = self.duplication_filtering(persona_category_filter_results)
+
+        return duplication_filter_results
+    
 def define_models():
     lemma = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma')
     classifier = pipeline('zero-shot-classification')
@@ -21,6 +142,7 @@ def define_models():
         'lemma': lemma,
         'clf': classifier,
     }
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
